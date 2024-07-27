b3bbe5183fc2167108b24da4768ed38ace9c3abe
Domi <dominik119017@gmail.com>
2024-07-27 19:17:58 +0200

    stegosaurus

--- .p10k.zsh
+++ .p10k.zsh
@@ -1672,3 +1672,6 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+
+[[ $- != *i* ]] && return
+fortune | cowsay -f stegosaurus
